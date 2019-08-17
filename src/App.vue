0e0e9aba4de2c40530143396810d1df746e0c9f8
OgheneMichael <zonalmeet@gmail.com>
2019-08-17 18:32:09 +0100

    added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>SWAPI</h1>
+    <router-view />
   </div>
 </template>
 
@@ -15,8 +16,32 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+
+img {
+  display: block;
+  width: 100%;
+  transition: all 0.3s ease;
+}
+
+.btn {
+  position: relative;
+  display: inline-block;
+  background: #d8d8d8;
+  font-size: 15px;
+  font-weight: 400;
+  padding: 8px 15px;
+  outline: none;
+  cursor: pointer;
+  transition: 0.2s ease;
+  color: #0e0e0e;
+  text-decoration: none;
 }
 </style>
