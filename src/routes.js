d9b427db9eac35764ec2a2530a4d786c491ab3c8
OgheneMichael <zonalmeet@gmail.com>
2019-08-20 07:27:50 +0100

    added new routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "./views/Home";
+import Starships from "./views/Starships";
+import Planets from "./views/Home";
+import Characters from "./views/Home";
 
 Vue.use(Router);
 
@@ -11,6 +14,21 @@ export default new Router({
 			path: "/",
 			name: "home",
 			component: Home
+		},
+		{
+			path: "/starships",
+			name: "starships",
+			component: Starships
+		},
+		{
+			path: "/planets",
+			name: "planets",
+			component: Planets
+		},
+		{
+			path: "/characters",
+			name: "characters",
+			component: Characters
 		}
 	]
 });
